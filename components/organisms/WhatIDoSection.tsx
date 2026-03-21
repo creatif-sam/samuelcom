20f438a0267533c072efc01110c2ccb663eab742
creatif-sam <samuel.creatiftech@gmail.com>
2026-03-21 15:58:38 +0000

    refactoring my hero section2

--- components/organisms/WhatIDoSection.tsx
+++ components/organisms/WhatIDoSection.tsx
@@ -68,7 +68,7 @@ const cardVariants = {
   visible: { 
     y: 0, 
     opacity: 1,
-    transition: { type: "spring", stiffness: 100 }
+    transition: { type: "spring" as const, stiffness: 100 }
   },
 };
 
